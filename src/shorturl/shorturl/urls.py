dd57ba49480e383263193ee6763a783666839963
Matias Banchoff <banchoff@gmail.com>
2023-05-25 10:53:30 -0500

    All templates extends project's base.html instead of app's base.html.

--- src/shorturl/shorturl/urls.py
+++ src/shorturl/shorturl/urls.py
@@ -20,7 +20,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
-#    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path("shortener/", include("shortener.urls")),
     path("shortener/", include("django.contrib.auth.urls")),
